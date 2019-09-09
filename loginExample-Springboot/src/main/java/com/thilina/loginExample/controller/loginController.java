6e78e3bb7bc7822fc0787d423f3b31acff570b43
Thilini-Thilakarathna <hdilhara@gmail.com>
2019-09-09 07:57:34 +0530

    chang loggin

--- loginExample-Springboot/src/main/java/com/thilina/loginExample/controller/loginController.java
+++ loginExample-Springboot/src/main/java/com/thilina/loginExample/controller/loginController.java
@@ -1,13 +1,19 @@
 package com.thilina.loginExample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.thilina.loginExample.servise.LoginServise;
+
 @Controller
 public class loginController {
+	
+	@Autowired
+	LoginServise serviseValidate;
 	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String loginPage() {
@@ -18,7 +24,7 @@ public class loginController {
 	public String welcomePage(@RequestParam String uname,@RequestParam String password,ModelMap model) {
 		model.put("m1", uname);
 		
-		if (uname.equalsIgnoreCase("thilina")&& password.equalsIgnoreCase("1234"))
+		if (serviseValidate.validateMe(uname, password))
 		return "welcome";
 		
 		else
