6f74a2c834b4f4009a6ee6f90c0a3c67e8b73cb5
Thilini-Thilakarathna <hdilhara@gmail.com>
2019-10-04 17:37:48 +0530

    add user detatils

--- spring-boot/springSecurity/src/main/java/com/thilina/springSecurity/controller/AppController.java
+++ spring-boot/springSecurity/src/main/java/com/thilina/springSecurity/controller/AppController.java
@@ -1,11 +1,20 @@
 package com.thilina.springSecurity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class AppController {
+
+
 
     @GetMapping("/")
     public  String getHomePage(){
@@ -13,7 +22,8 @@ public class AppController {
     }
 
     @GetMapping("/webprivate")
-    public  String getWebprivatePage(){
+    public  String getWebprivatePage(Principal principal,Model model){
+        model.addAttribute("uname",principal.getName());
         return "webprivate";
     }
 
@@ -27,4 +37,8 @@ public class AppController {
         return "login";
     }
 
+//    @Bean
+//    public String currentUserName(Principal principal) {
+//        return principal.getName();
+//    }
 }
